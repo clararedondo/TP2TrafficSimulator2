86bd253122da08f5aa65a3ccb862d14e012bfa2d
gemmapercy2000 <89588849+gemmapercy2000@users.noreply.github.com>
2022-04-19 01:45:41 +0200

    aqui:(

--- src/simulator/view/MapComponent.java
+++ src/simulator/view/MapComponent.java
@@ -179,26 +179,8 @@ public class MapComponent extends JPanel implements TrafficSimObserver {
 			int w, int h, //
 			Color lineColor, Color arrowColor) {
 
-		int dx = x2 - x1, dy = y2 - y1;
-		double D = Math.sqrt(dx * dx + dy * dy);
-		double xm = D - w, xn = xm, ym = h, yn = -h, x;
-		double sin = dy / D, cos = dx / D;
 
-		x = xm * cos - ym * sin + x1;
-		ym = xm * sin + ym * cos + y1;
-		xm = x;
 
-		x = xn * cos - yn * sin + x1;
-		yn = xn * sin + yn * cos + y1;
-		xn = x;
-
-		int[] xpoints = { x2, (int) xm, (int) xn };
-		int[] ypoints = { y2, (int) ym, (int) yn };
-
-		g.setColor(lineColor);
-		g.drawLine(x1, y1, x2, y2);
-		g.setColor(arrowColor);
-		g.fillPolygon(xpoints, ypoints, 3);
 	}
 
 	// loads an image from a file
