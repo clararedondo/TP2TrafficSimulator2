e283b3d2156a550e24139da11503fdb861dbb75f
Clara Redondo <claralredondo@gmail.com>
2022-04-19 01:57:24 +0200

    Revert "aqui:("
    This reverts commit 86bd253122da08f5aa65a3ccb862d14e012bfa2d.

--- src/simulator/view/MapByRoadComponent.java
+++ src/simulator/view/MapByRoadComponent.java
@@ -48,7 +48,7 @@ public class MapByRoadComponent extends JComponent implements TrafficSimObserver
 	}
 	
 	private void initGUI() {
-		_car = loadImage("car.png");
+		_car = loadImage("car_front.png");
 		_cloud = loadImage("cloud.png");
 		_rain = loadImage("rain.png");
 		_storm = loadImage("storm.png");
@@ -136,26 +136,8 @@ public class MapByRoadComponent extends JComponent implements TrafficSimObserver
 
 	private void drawLineWithArrow(Graphics g, int x1, int y1, int x2, int y2, int i, int j, Color roadColor,
 			Color arrowColor) {
-		int dx = x2 - x1, dy = y2 - y1;
-		double D = Math.sqrt(dx * dx + dy * dy);
-		double xm = D - w, xn = xm, ym = h, yn = -h, x;
-		double sin = dy / D, cos = dx / D;
-
-		x = xm * cos - ym * sin + x1;
-		ym = xm * sin + ym * cos + y1;
-		xm = x;
-
-		x = xn * cos - yn * sin + x1;
-		yn = xn * sin + yn * cos + y1;
-		xn = x;
-
-		int[] xpoints = { x2, (int) xm, (int) xn };
-		int[] ypoints = { y2, (int) ym, (int) yn };
-
-		g.setColor(lineColor);
-		g.drawLine(x1, y1, x2, y2);
-		g.setColor(arrowColor);
-		g.fillPolygon(xpoints, ypoints, 3);		
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void drawVehicles(Graphics g) {
